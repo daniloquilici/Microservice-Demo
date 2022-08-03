3be7319d9f6074c687e30ca7511895e4f7a0bfd9
Danilo Quilici <daniloquilici@gmail.com>
2022-08-02 21:32:23 -0300

    Finished items controller and add SuppressAsyncSuffixInActionNames = false; in startup.

--- quilici.Catalog/src/quilici.Catalog.Service/Controllers/ItensController.cs
+++ quilici.Catalog/src/quilici.Catalog.Service/Controllers/ItensController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using quilici.Catalog.Service.Entities;
 using quilici.Catalog.Service.Repositories;
 using System;
 using System.Collections.Generic;
@@ -22,7 +23,7 @@ namespace quilici.Catalog.Service.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ItemDto>> GetByIdAsync(Guid id) 
+        public async Task<ActionResult<ItemDto>> GetByIdAsync(Guid id)
         {
             var item = await itemsRepository.GetAsync(id);
 
@@ -30,6 +31,51 @@ namespace quilici.Catalog.Service.Controllers
                 return NotFound();
 
             return Ok(item.AsDto());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(CreateItemDto createItemDto)
+        {
+            var item = new Item
+            {
+                Name = createItemDto.name,
+                Description = createItemDto.description,
+                Price = createItemDto.price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            await itemsRepository.CreateAsync(item);
+
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutAsync(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = await itemsRepository.GetAsync(id);
+            if (existingItem == null)
+                return NotFound();
+
+            existingItem.Name = itemDto.name;
+            existingItem.Description = itemDto.description;
+            existingItem.Price = itemDto.price;
+
+            await itemsRepository.UpdateAsync(existingItem);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(Guid id)
+        {
+            var item = await itemsRepository.GetAsync(id);
+            if (item == null)
+                return NotFound();
+
+            await itemsRepository.RemoveAsync(item);
+
+            return NoContent();
+
         }
     }
 }
--- quilici.Catalog/src/quilici.Catalog.Service/Startup.cs
+++ quilici.Catalog/src/quilici.Catalog.Service/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace quilici.Catalog.Service
 {
@@ -27,7 +20,10 @@ namespace quilici.Catalog.Service
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers(options =>
+            {
+                options.SuppressAsyncSuffixInActionNames = false;
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "quilici.Catalog.Service", Version = "v1" });
