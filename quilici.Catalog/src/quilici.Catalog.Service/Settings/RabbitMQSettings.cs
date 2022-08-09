c05eb680b028704a9040de95f3c68e2be74e3726
Danilo Quilici <daniloquilici@gmail.com>
2022-08-09 10:55:16 -0300

    Implement RabbitMQ

---
+++ quilici.Catalog/src/quilici.Catalog.Service/Settings/RabbitMQSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace quilici.Catalog.Service.Settings
+{
+    public class RabbitMQSettings
+    {
+        public string Host { get; init; }
+    }
+}
--- quilici.Catalog/src/quilici.Catalog.Service/Startup.cs
+++ quilici.Catalog/src/quilici.Catalog.Service/Startup.cs
@@ -1,3 +1,5 @@
+using MassTransit;
+using MassTransit.Definition;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -5,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using quilici.Catalog.Service.Entities;
+using quilici.Catalog.Service.Settings;
 using quilici.Common.MongoDB;
 using quilici.Common.Settings;
 
@@ -27,6 +30,18 @@ namespace quilici.Catalog.Service
             serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
             services.AddMongo().AddMongoRepository<Item>("items");
+
+            services.AddMassTransit(x =>
+            {
+                x.UsingRabbitMq((context, configurator) =>
+                {
+                    var rabbitMQSettings = Configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                    configurator.Host(rabbitMQSettings.Host);
+                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                });
+            });
+
+            services.AddMassTransitHostedService();
 
             services.AddControllers(options =>
             {
