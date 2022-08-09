a01937a8a7a5afd6ff1ef75205e7fd8f5157cfbb
Danilo Quilici <daniloquilici@gmail.com>
2022-08-08 22:13:33 -0300

    add class contract

---
+++ quilici.Catalog/src/quilici.Catalog.Contracts/Contracts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace quilici.Catalog.Contracts
+{
+    public class Contracts
+    {
+        public record CatalogItemCreated(Guid itemId, string name, string description);
+
+        public record CatalogItemUpdate(Guid itemId, string name, string description);
+
+        public record CatalogItemDelete(Guid itemId);
+    }
+}
