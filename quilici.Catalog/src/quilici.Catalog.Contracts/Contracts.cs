69d4d276999d8629b407d05e2c81d8fe060d9444
Danilo Quilici <daniloquilici@gmail.com>
2022-08-09 20:23:41 -0300

    I wrote the name the class wrong

--- quilici.Catalog/src/quilici.Catalog.Contracts/Contracts.cs
+++ quilici.Catalog/src/quilici.Catalog.Contracts/Contracts.cs
@@ -4,7 +4,7 @@ namespace quilici.Catalog.Contracts
 {
     public record CatalogItemCreated(Guid itemId, string name, string description);
 
-    public record CatalogItemUpdate(Guid itemId, string name, string description);
+    public record CatalogItemUpdated(Guid itemId, string name, string description);
 
-    public record CatalogItemDelete(Guid itemId);
+    public record CatalogItemDeleted(Guid itemId);
 }
